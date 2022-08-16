5d0d67bf7b0167274bb9eede7bc7b624246c778c
Jonas <jonas.brand@bluewin.ch>
2022-08-16 22:55:34 +0200

    added type and function declaration for configuring mapped memory

--- src/memory/memory.h
+++ src/memory/memory.h
@@ -3,19 +3,28 @@
 
 #include <stdint.h>
 
-//type to store momory adresses of vm
-typedef uint16_t addr_t;
+//type to store memory adresses of vm
+typedef uint16_t dtptr_t;
+
+//type to store memory page address of vm
+typedef uint8_t pgptr_t;
 
 //fech memory functions
-uint8_t mem_fech_8(addr_t addr);
-uint16_t mem_fech_16(addr_t addr);
-uint32_t mem_fech_32(addr_t addr);
-uint64_t mem_fech_64(addr_t addr);
+uint8_t mem_fech_8(dtptr_t addr);
+uint16_t mem_fech_16(dtptr_t addr);
+uint32_t mem_fech_32(dtptr_t addr);
+uint64_t mem_fech_64(dtptr_t addr);
 
 //write memory functions
-void mem_write_8(addr_t addr, uint8_t data);
-void mem_write_16(addr_t addr, uint8_t data);
-void mem_write_32(addr_t addr, uint8_t data);
-void mem_write_64(addr_t addr, uint8_t data);
+void mem_write_8(dtptr_t addr, uint8_t data);
+void mem_write_16(dtptr_t addr, uint8_t data);
+void mem_write_32(dtptr_t addr, uint8_t data);
+void mem_write_64(dtptr_t addr, uint8_t data);
+
+//function type for actions to be taken when acessing mapped memory
+typedef void (*memacc_act_t)(dtptr_t addr);
+
+//function for configuring maped memory
+void mem_map(pgptr_t page, memacc_act_t on_fech, memacc_act_t on_write);
 
 #endif //! __MEMORY__
