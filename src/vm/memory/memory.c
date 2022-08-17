093c2aea224cbd10b248acd0edb3e192de3f5e81
Jonas <jonas.brand@bluewin.ch>
2022-08-17 18:13:32 +0200

    implemented mem_fech_16 function

--- src/vm/memory/memory.c
+++ src/vm/memory/memory.c
@@ -1,1 +1,16 @@
-#include "memory.h"
+#include "mem_access.h"
+#include "mem_config.h"
+
+static uint8_t memory[0xFFFF];
+
+//=======================================================================================================fech memory functions
+uint16_t mem_fech_16(dtptr_t addr)
+{
+    return *(uint16_t*)(memory + addr);
+}
+
+uint32_t mem_fech_32(dtptr_t addr);
+
+//=======================================================================================================write memory functions
+void mem_write_16(dtptr_t addr, uint8_t data);
+void mem_write_32(dtptr_t addr, uint8_t data);
