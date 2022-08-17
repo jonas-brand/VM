aaf12f7595b937fd8146b12c82b9b57b355098a3
Jonas <jonas.brand@bluewin.ch>
2022-08-17 18:14:24 +0200

    implemented mem_fech_32 function

--- src/vm/memory/memory.c
+++ src/vm/memory/memory.c
@@ -9,7 +9,10 @@ uint16_t mem_fech_16(dtptr_t addr)
     return *(uint16_t*)(memory + addr);
 }
 
-uint32_t mem_fech_32(dtptr_t addr);
+uint32_t mem_fech_32(dtptr_t addr)
+{
+    return *(uint32_t*)(memory + addr);
+}
 
 //=======================================================================================================write memory functions
 void mem_write_16(dtptr_t addr, uint8_t data);
