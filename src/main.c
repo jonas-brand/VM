b2223acf0a68713cc575c9aff4c823ae153b35a7
Jonas <jonas.brand@bluewin.ch>
2022-08-18 09:00:34 +0200

    added tests for mem_write functions

--- src/main.c
+++ src/main.c
@@ -8,7 +8,10 @@ int main(void)
     if(! mem_load(0x0, 14, "Hello, World!"))
         printf("LOAD!");
 
-    if(! mem_print(0x0, 0xFF))
+    mem_write_16(0xF, 0x1234);
+    mem_write_32(0x12, 0x87654321);
+
+    if(! mem_print(0x0, 30))
         printf("PRINT!");
 
     return EXIT_SUCCESS;
