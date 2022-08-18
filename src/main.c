62bd198281f6708a45685f3f4ac83d7369dfd004
Jonas <jonas.brand@bluewin.ch>
2022-08-18 08:25:49 +0200

    added first test of mem_print function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "vm/memory/mem_access.h"
+#include "vm/memory/mem_config.h"
 
 int main(void)
 {
-    printf("Hello, World!");
+    mem_print(0xFF, 10);
+
     return EXIT_SUCCESS;
 }
