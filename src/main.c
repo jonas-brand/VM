bc5867e04ab716abfabd755d499093e95c69bf18
Jonas <jonas.brand@bluewin.ch>
2022-08-18 09:06:28 +0200

    added tests for mem_fech functions

--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@ int main(void)
     mem_write_16(0xF, 0x1234);
     mem_write_32(0x12, 0x87654321);
 
+    printf("fech_16 at 0xF: %x\n", mem_fech_16(0xF));
+    printf("fech_16 at 0x14: %x\n", mem_fech_16(0x14));
+    printf("fech_32 at 0x12: %x\n", mem_fech_32(0x12));
+    printf("fech_32 at 0xF: %x\n", mem_fech_32(0xF));
+
     if(! mem_print(0x0, 30))
         printf("PRINT!");
 
