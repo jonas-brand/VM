bf8165799b2ddae322ee235bda2ef30927b3badd
Jonas <jonas.brand@bluewin.ch>
2022-08-18 09:35:54 +0200

    removed tests

--- src/main.c
+++ src/main.c
@@ -5,19 +5,5 @@
 
 int main(void)
 {
-    if(! mem_load(0x0, 14, "Hello, World!"))
-        printf("LOAD!");
-
-    mem_write_16(0xF, 0x1234);
-    mem_write_32(0x12, 0x87654321);
-
-    printf("fech_16 at 0xF: %x\n", mem_fech_16(0xF));
-    printf("fech_16 at 0x14: %x\n", mem_fech_16(0x14));
-    printf("fech_32 at 0x12: %x\n", mem_fech_32(0x12));
-    printf("fech_32 at 0xF: %x\n", mem_fech_32(0xF));
-
-    if(! mem_print(0x0, 30))
-        printf("PRINT!");
-
     return EXIT_SUCCESS;
 }
