b5018c4a383f5e01727c83e9c053333b2b6e3303
Jonas <jonas.brand@bluewin.ch>
2022-08-19 08:37:27 +0200

    added cpu struct

--- src/vm/cpu.c
+++ src/vm/cpu.c
@@ -1,1 +1,33 @@
 #include "cpu.h"
+#include "memory/mem_access.h"
+
+//type for pointers to general perpose registers
+typedef uint16_t regptr_t;
+
+//struct for holding cpu information
+static struct
+{
+    uint8_t gp_regs[16];    //memory for general perpose registers
+
+    //inecies of general perpose registers
+    enum
+    {
+        GPR1 = 0,
+        GPR1L = 0,
+        GPR1H = 2,
+        GPR2 = 4,
+        GPR2L = 4,
+        GPR2H = 6,
+        GPR3 = 8,
+        GPR3L = 8,
+        GPR3H = 10,
+        GPR4 = 12,
+        GPR4L = 12,
+        GPR4H = 14
+    }gpr;
+
+    uint16_t status;    //status register
+    uint16_t istr_ptr;  //instruction pointer
+    uint16_t stk_ptr;   //stack pointer
+    uint16_t ivt_ptr;   //pointer to interrupt vector table
+}cpu;
--- src/vm/cpu.h
+++ src/vm/cpu.h
@@ -1,1 +1,4 @@
-#include "memory/mem_access.h"
+#ifndef __CPU_H__
+#define __CPU_H__
+
+#endif //! __CPU_H__
