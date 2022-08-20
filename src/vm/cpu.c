9d3bec9309f796d4303517a8a826228149bd7be9
Jonas <jonas.brand@bluewin.ch>
2022-08-20 13:12:10 +0200

    added regptr types

--- src/vm/cpu.c
+++ src/vm/cpu.c
@@ -2,30 +2,31 @@
 #include "memory/mem_access.h"
 #include <stdbool.h>
 
-//type for pointers to general perpose registers
-typedef uint16_t regptr_t;
+//types for pointers to general perpose registers
+typedef enum
+{
+    gpr0 = 0,
+    gpr1 = 4,
+    gpr2 = 8,
+    gpr3 = 12
+}gpr32ptr_t;
+
+typedef enum
+{
+    gpr0l = gpr0,
+    gpr0h = gpr0 + 2,
+    gpr1l = gpr1,
+    gpr1h = gpr1 + 2,
+    gpr2l = gpr2,
+    gpr2h = gpr2 + 2,
+    gpr3l = gpr3,
+    gpr3h = gpr3 + 2
+}gpr16ptr_t;
 
 //struct for holding cpu information
 static struct
 {
     uint8_t gp_regs[16];    //memory for general perpose registers
-
-    //inecies of general perpose registers
-    enum
-    {
-        GPR1 = 0,
-        GPR1L = 0,
-        GPR1H = 2,
-        GPR2 = 4,
-        GPR2L = 4,
-        GPR2H = 6,
-        GPR3 = 8,
-        GPR3L = 8,
-        GPR3H = 10,
-        GPR4 = 12,
-        GPR4L = 12,
-        GPR4H = 14
-    }gpr;
 
     uint16_t status;    //status register
     uint16_t ip;  //instruction pointer
