e1af5709d4f4980c7e799ace685d50f5573d9bd5
Jonas <jonas.brand@bluewin.ch>
2022-08-21 13:29:28 +0200

    added push and pop instructions

--- src/vm/cpu.c
+++ src/vm/cpu.c
@@ -48,8 +48,8 @@ enum
 uint8_t regs[22];
 
 //macros for accessing registers
-#define R16(reg) *(uint16_t*)(regs + (r16ptr_t)arg)
-#define R32(reg) *(uint32_t*)(regs + (r32ptr_t)arg)
+#define R16(reg) *(uint16_t*)(regs + (r16ptr_t)reg)
+#define R32(reg) *(uint32_t*)(regs + (r32ptr_t)reg)
 
 //=======================================================================================functions for parsing instructions
 static arg_type_t parse_arg1(instr_t instr)
@@ -81,57 +81,72 @@ void instr_exec(instr_t instr, uint32_t arg1, uint32_t arg2)
 
         //move l8 into lower byte of r16
         case INSTR(l8, r16, mov):
-            regs[(r16ptr_t)arg2] = (uint8_t)arg1;
+            R16(arg2) = (uint8_t)arg1;
             break;
 
         //move m8 into lower byte of r16
         case INSTR(m8, r16, mov):
-            regs[(r16ptr_t)arg2] = mem_fech_8((dtptr_t)arg1);
+            R16(arg2) = mem_fech_8((dtptr_t)arg1);
             break;
 
         //move lower byte of r16 into m8
         case INSTR(r16, m8, mov):
-            mem_write_8((dtptr_t)arg2, (uint8_t)regs[(r16ptr_t)arg1]);
+            mem_write_8((dtptr_t)arg2, (uint8_t)R16(arg1));
             break;
 
         //move l16 into r16
         case INSTR(l16, r16, mov):
-            *(uint16_t*)(regs + (r16ptr_t)arg2) = (uint16_t)arg1;
+            R16(arg2) = (uint16_t)arg1;
             break;
 
         //move m16 into r16
         case INSTR(m16, r16, mov):
-            *(uint16_t*)(regs + (r16ptr_t)arg2) = mem_fech_16((dtptr_t)arg1);
+            R16(arg2) = mem_fech_16((dtptr_t)arg1);
             break;
 
         //move r16 into m16
         case INSTR(r16, m16, mov):
-            mem_write_16((dtptr_t)arg2, *(uint16_t*)(regs + (r16ptr_t)arg1));
+            mem_write_16((dtptr_t)arg2, R16(arg1));
             break;
 
         //move l32 into r32
         case INSTR(l32, r32, mov):
-            *(uint32_t*)(regs + (r32ptr_t)arg2) = (uint32_t)arg1;
+            R32(arg2) = (uint32_t)arg1;
             break;
 
         //move m32 into r32
         case INSTR(m32, r32, mov):
-            *(uint32_t*)(regs + (r32ptr_t)arg2) = mem_fech_32((dtptr_t)arg1);
+            R32(arg2) = mem_fech_32((dtptr_t)arg1);
             break;
 
         //move r32 into m32
         case INSTR(r32, m32, mov):
-            mem_write_32((dtptr_t)arg2, *(uint32_t*)(regs + (r32ptr_t)arg1));
+            mem_write_32((dtptr_t)arg2, R32(arg1));
             break;
 
         //push r16 onto the stack
         case INSTR(r16, none, psh):
-            mem_write_16(*(dtptr_t*)(regs + (r16ptr_t)ip), (uint16_t)arg1);
+            mem_write_16((dtptr_t)R16(ip), R16(arg1));
+            break;
+
+        //push r32 onto the stack
+        case INSTR(r32, none, psh):
+            mem_write_32((dtptr_t)R16(ip), R32(arg1));
+            break;
+
+        //pop 2 bytes of the stack into r16
+        case INSTR(r16, none, pop):
+            R16(arg1) = mem_fech_16((dtptr_t)R16(ip));
+            break;
+
+        //pop 4 bytes of the stack into r32
+        case INSTR(r32, none, pop):
+            R32(arg1) = mem_fech_32((dtptr_t)R16(ip));
             break;
 
         //error, jump to panic interrupt
         default:
-            regs[ip] = (regs[stat] << 8) + pai;
+            R16(ip) = (R16(stat) << 8) + pai;
             break;
     }
 }
