f3405433560f8045d3fb6a450121ad9f34d856e5
Jonas <jonas.brand@bluewin.ch>
2022-08-21 21:10:16 +0200

    added DT macros

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "vm/cpu.h"
+#include "vm/memory/mem_config.h"
+
+#define PRGM_SIZE 30
+
+#define DT16(dt) (uint8_t)dt, (uint8_t)(dt >> 8)
+#define DT16(dt) (uint8_t)dt, (uint8_t)(dt >> 8), (uint8_t)(dt >> 16), (uint8_t)(dt >> 24)
+
+static uint8_t prgm[PRGM_SIZE] =
+{
+    
+};
 
 int main(void)
 {
